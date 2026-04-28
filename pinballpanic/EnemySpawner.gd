d48f518634343a63fc94d3ce107aa03bb0d3d771
KOZUSEGI <hauntedvanam@gmail.com>
2026-04-28 16:07:32 +0700

    added jumlah spawn kalau waveganti

--- pinballpanic/EnemySpawner.gd
+++ pinballpanic/EnemySpawner.gd
@@ -1,28 +1,33 @@
 extends Node2D
 
 @export var enemy_scene: PackedScene
-@onready var spawn_point = $spawn_points/Marker2D
-
+@onready var spawn_point = [
+	$spawn_points/Marker2D,
+	$spawn_points/Marker2D2,
+	$spawn_points/Marker2D3]
 var enemy_per_wave = 5
 var spawned = 0
 var wave = 1
-var max_wave = 30
-
+var max_wave = 15
+var spawn_amount = 1
+var current_wave = 1
 func _ready():
-	await get_tree().create_timer(1.5).timeout
+	await get_tree().create_timer(2.5).timeout
+	
 	start_wave()
 
 func start_wave():
 	spawned = 0
-	print("=== START WAVE", wave, "===")
+	update_jumlah_enemy()
+	print(" start wave", wave, "")
 	spawn_next()
 
 func spawn_next():
 	# STOP kalau sudah cukup spawn
 	if spawned >= enemy_per_wave:
-		print("Wave selesai")
+		print("Wave end")
 
-		await get_tree().create_timer(5.0).timeout
+		await get_tree().create_timer(7.0).timeout
 
 		wave += 1
 		stop_wave()
@@ -32,22 +37,31 @@ func spawn_next():
 	# spawn enemy
 	spawn_enemy()
 
-	# lanjut lagi (INI YANG KAMU KURANG SEBELUMNYA)
-	await get_tree().create_timer(1.0).timeout
+	await get_tree().create_timer(3.5).timeout
 	spawn_next()
 
 func spawn_enemy():
-	var enemy = enemy_scene.instantiate()
-	enemy.global_position = spawn_point.global_position
-	add_child(enemy)
+	for i in range(spawn_amount):
+		if spawned >= enemy_per_wave:
+			return
+		var enemy = enemy_scene.instantiate()
+		var points = spawn_point [i % spawn_point.size()]
+		enemy.global_position = points.global_position
+		add_child(enemy)
 
 	spawned += 1
-	await get_tree().create_timer(2.0).timeout
+	await get_tree().create_timer(3.0).timeout
 	print("enemy spawned:", spawned)
 
 func stop_wave():
 	if wave >= max_wave:
 		game_over()
-
+func update_jumlah_enemy():
+	if wave <=3:
+		spawn_amount = 1
+	elif wave <= 9:
+		spawn_amount = 2
+	else:
+		spawn_amount = 3		
 func game_over():
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
